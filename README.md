15a3736089098b208b8f7dca783984af5cee89b2
tpatatoev <tpatata@gmail.com>
2024-05-12 23:17:38 +0300

    test commit

--- README.md
+++
@@ -1,1 +0,0 @@
-Package allows to interact with products.mti.ua through rest api. Api enables to get content for distributed products
--- tests/test.php
+++ tests/test.php
@@ -5,3 +5,5 @@ namespace MTI\DealerApi\V2;
 use MTI\DealerApi\V2\Controllers\MainController;
 
 MainController::handleRequest();
+
+//test commit
